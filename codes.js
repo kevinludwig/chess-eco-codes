2421e0711073e13bdc81603cd1efa72199eaa2cf
Kevin Ludwig <kevin.ludwig@gmail.com>
2017-06-28 19:19:39 -0700

    A26-A32

--- codes.js
+++ codes.js
@@ -708,5 +708,95 @@ module.exports = [
         code: "A25",
         name: "English, closed system (without ...d6)",
         moves: "1. c4 e5 2. Nc3 Nc6 3. g3 g6 4. Bg2 Bg7 5. d3"
+    },
+    {
+        code: "A26",
+        name: "English, closed system",
+        moves: "1. c4 e5 2. Nc3 Nc6 3. g3 g6 4. Bg2 Bg7 5. d3 d6"
+    },
+    {
+        code: "A26",
+        name: "English, Botvinnik system",
+        moves: "1. c4 e5 2. Nc3 Nc6 3. g3 g6 4. Bg2 Bg7 5. d3 d6 6. e4"
+    },
+    {
+        code: "A27",
+        name: "English, three knights system",
+        moves: "1. c4 e5 2. Nc3 Nc6 3. Nf3"
+    },
+    {
+        code: "A28",
+        name: "English, four knights system",
+        moves: "1. c4 e5 2. Nc3 Nc6 3. Nf3 Nf6"
+    },
+    {
+        code: "A28",
+        name: "English, Nenarokov variation",
+        moves: "1. c4 e5 2. Nc3 Nc6 3. Nf3 Nf6 4. d4 exd4 5. Nxd4 Bb4 6. Bg5 h6 7. Bh4 Bxc3+ 8. bxc3 Ne5"
+    },
+    {
+        code: "A28",
+        name: "English, Bradley Beach variation",
+        moves: "1. c4 e5 2. Nc3 Nc6 3. Nf3 Nf6 4. d4 e4"
+    },
+    {
+        code: "A28",
+        name: "English, four knights, Nimzovich variation",
+        moves: "1. c4 e5 2. Nc3 Nc6 3. Nf3 Nf6 4. e4"
+    },
+    {
+        code: "A28",
+        name: "English, four knights, Marini variation",
+        moves: "1. c4 e5 2. Nc3 Nc6 3. Nf3 Nf6 4. a3"
+    },
+    {
+        code: "A28",
+        name: "English, four knights, Capablanca variation",
+        moves: "1. c4 e5 2. Nc3 Nc6 3. Nf3 Nf6 4. d3"
+    },
+    {
+        code: "A28",
+        name: "English, four knights, 4.e3",
+        moves: "1. c4 e5 2. Nc3 Nc6 3. Nf3 Nf6 4. e3"
+    },
+    {
+        code: "A28",
+        name: "English, four knights, Stean variation",
+        moves: "1. c4 e5 2. Nc3 Nc6 3. Nf3 Nf6 4. e3 Bb4 5. Qc2 O-O 6. Nd5 Re8 7. Qf5"
+    },
+    {
+        code: "A28",
+        name: "English, four knights, Romanishin variation",
+        moves: "1. c4 e5 2. Nc3 Nc6 3. Nf3 Nf6 4. e3 Bb4 5. Qc2 Bxc3"
+    },
+    {
+        code: "A29",
+        name: "English, four knights, kingside fianchetto",
+        moves: "1. c4 e5 2. Nc3 Nc6 3. Nf3 Nf6 4. g3"
+    },
+    {
+        code: "A30",
+        name: "English, symmetrical variation",
+        moves: "1. c4 c5"
+    },
+    {
+        code: "A30",
+        name: "English, symmetrical, hedgehog system",
+        moves: "1. c4 c5 2. Nf3 Nf6 3. g3 b6 4. Bg2 Bb7 5. O-O e6 6. Nc3 Be7"
+    },
+    {
+        code: "A30",
+        name: "English, symmetrical, hedgehog, flexible formation",
+        moves: "1. c4 c5 2. Nf3 Nf6 3. g3 b6 4. Bg2 Bb7 5. O-O e6 6. Nc3 Be7 7. d4 cxd4 8. Qxd4 d6 9. Rd1 a6 10. b3 Nbd7"
+    },
+    {
+        code: "A31",
+        name: "English, symmetrical, Benoni formation",
+        moves: "1. c4 c5 2. Nf3 Nf6 3. d4"
+    },
+    {
+        code: "A32",
+        name: "English, symmetrical variation",
+        moves: "1. c4 c5 2. Nf3 Nf6 3. d4 cxd4 4. Nxd4 e6"
     }
 ];
