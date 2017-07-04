a5d78d980a64509c1235a12f61dc872dc7e38ce4
Kevin Ludwig <kevin.ludwig@gmail.com>
2017-07-03 22:22:08 -0700

    B00-B05

--- B.js
+++ B.js
@@ -1,5 +1,395 @@
 module.exports = [
     {
+        code: "B00",
+        name: "King's pawn opening",
+        moves: "1. e4"
+    },
+    {
+        code: "B00",
+        name: "Hippopotamus defence",
+        moves: "1. e4 Nh6 2. d4 g6 3. c4 f6"
+    },
+    {
+        code: "B00",
+        name: "Corn stalk defence",
+        moves: "1. e4 a5"
+    },
+    {
+        code: "B00",
+        name: "Lemming defence",
+        moves: "1. e4 Na6"
+    },
+    {
+        code: "B00",
+        name: "Fred",
+        moves: "1. e4 f5"
+    },
+    {
+        code: "B00",
+        name: "Barnes defence",
+        moves: "1. e4 f6"
+    },
+    {
+        code: "B00",
+        name: "Fried fox defence",
+        moves: "1. e4 f6 2. d4 Kf7"
+    },
+    {
+        code: "B00",
+        name: "Carr's defence",
+        moves: "1. e4 h6"
+    },
+    {
+        code: "B00",
+        name: "Reversed Grob (Borg/Basman defence/macho Grob)",
+        moves: "1. e4 g5"
+    },
+    {
+        code: "B00",
+        name: "St. George (Baker) defence",
+        moves: "1. e4 a6"
+    },
+    {
+        code: "B00",
+        name: "Owen defence",
+        moves: "1. e4 b6"
+    },
+    {
+        code: "B00",
+        name: "Guatemala defence",
+        moves: "1. e4 b6 2. d4 Ba6"
+    },
+    {
+        code: "B00",
+        name: "KP, Nimzovich defence",
+        moves: "1. e4 Nc6"
+    },
+    {
+        code: "B00",
+        name: "KP, Nimzovich defence, Wheeler gambit",
+        moves: "1. e4 Nc6 2. b4 Nxb4 3. c3 Nc6 4. d4"
+    },
+    {
+        code: "B00",
+        name: "KP, Nimzovich defence",
+        moves: "1. e4 Nc6 2. Nf3"
+    },
+    {
+        code: "B00",
+        name: "KP, Colorado counter",
+        moves: "1. e4 Nc6 2. Nf3 f5"
+    },
+    {
+        code: "B00",
+        name: "KP, Nimzovich defence",
+        moves: "1. e4 Nc6 2. d4"
+    },
+    {
+        code: "B00",
+        name: "KP, Nimzovich defence, Marshall gambit",
+        moves: "1. e4 Nc6 2. d4 d5 3. exd5 Qxd5 4. Nc3"
+    },
+    {
+        code: "B00",
+        name: "KP, Nimzovich defence, Bogolyubov variation",
+        moves: "1. e4 Nc6 2. d4 d5 3. Nc3"
+    },
+    {
+        code: "B00",
+        name: "KP, Neo-Mongoloid defence",
+        moves: "1. e4 Nc6 2. d4 f6"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian (centre counter) defence",
+        moves: "1. e4 d5"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian defence, Lasker variation",
+        moves: "1. e4 d5 2. exd5 Qxd5 3. Nc3 Qa5 4. d4 Nf6 5. Nf3 Bg4 6. h3"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian defence",
+        moves: "1. e4 d5 2. exd5 Qxd5 3. Nc3 Qa5 4. d4 Nf6 5. Nf3 Bf5"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian defence, Gruenfeld variation",
+        moves: "1. e4 d5 2. exd5 Qxd5 3. Nc3 Qa5 4. d4 Nf6 5. Nf3 Bf5 6. Ne5 c6 7. g4"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian, Anderssen counter-attack",
+        moves: "1. e4 d5 2. exd5 Qxd5 3. Nc3 Qa5 4. d4 e5"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian, Anderssen counter-attack orthodox attack",
+        moves: "1. e4 d5 2. exd5 Qxd5 3. Nc3 Qa5 4. d4 e5 5. dxe5 Bb4 6. Bd2 Nc6 7. Nf3"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian, Anderssen counter-attack, Goteborg system",
+        moves: "1. e4 d5 2. exd5 Qxd5 3. Nc3 Qa5 4. d4 e5 5. Nf3"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian, Anderssen counter-attack, Collijn variation",
+        moves: "1. e4 d5 2. exd5 Qxd5 3. Nc3 Qa5 4. d4 e5 5. Nf3 Bg4"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian, Mieses-Kotrvc gambit",
+        moves: "1. e4 d5 2. exd5 Qxd5 3. Nc3 Qa5 4. b4"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian, Pytel-Wade variation",
+        moves: "1. e4 d5 2. exd5 Qxd5 3. Nc3 Qd6"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian defence",
+        moves: "1. e4 d5 2. exd5 Nf6"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian, Icelandic gambit",
+        moves: "1. e4 d5 2. exd5 Nf6 3. c4 e6"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian gambit",
+        moves: "1. e4 d5 2. exd5 Nf6 3. c4 c6"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian defence",
+        moves: "1. e4 d5 2. exd5 Nf6 3. d4"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian, Marshall variation",
+        moves: "1. e4 d5 2. exd5 Nf6 3. d4 Nxd5"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian, Kiel variation",
+        moves: "1. e4 d5 2. exd5 Nf6 3. d4 Nxd5 4. c4 Nb4"
+    },
+    {
+        code: "B01",
+        name: "Scandinavian, Richter variation",
+        moves: "1. e4 d5 2. exd5 Nf6 3. d4 g6"
+    },
+    {
+        code: "B02",
+        name: "Alekhine's defence",
+        moves: "1 e4 Nf6"
+    },
+    {
+        code: "B02",
+        name: "Alekhine's defence, Scandinavian variation",
+        moves: "1. e4 Nf6 2. Nc3 d5"
+    },
+    {
+        code: "B02",
+        name: "Alekhine's defence, Spielmann variation",
+        moves: "1. e4 Nf6 2. Nc3 d5 3. e5 Nfd7 4. e6"
+    },
+    {
+        code: "B02",
+        name: "Alekhine's defence, Maroczy variation",
+        moves: "1. e4 Nf6 2. d3"
+    },
+    {
+        code: "B02",
+        name: "Alekhine's defence, Krejcik variation",
+        moves: "1. e4 Nf6 2. Bc4"
+    },
+    {
+        code: "B02",
+        name: "Alekhine's defence, Mokele Mbembe (Buecker) variation",
+        moves: "1. e4 Nf6 2. e5 Ne4"
+    },
+    {
+        code: "B02",
+        name: "Alekhine's defence, Brooklyn defence",
+        moves: "1. e4 Nf6 2. e5 Ng8"
+    },
+    {
+        code: "B02",
+        name: "Alekhine's defence",
+        moves: "1. e4 Nf6 2. e5 Nd5"
+    },
+    {
+        code: "B02",
+        name: "Alekhine's defence, Kmoch variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. Bc4 Nb6 4. Bb3 c5 5. d3"
+    },
+    {
+        code: "B02",
+        name: "Alekhine's defence, Saemisch attack",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. Nc3"
+    },
+    {
+        code: "B02",
+        name: "Alekhine's defence, Welling variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. b3"
+    },
+    {
+        code: "B02",
+        name: "Alekhine's defence",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. c4"
+    },
+    {
+        code: "B02",
+        name: "Alekhine's defence, Steiner variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. c4 Nb6 4. b3"
+    },
+    {
+        code: "B02",
+        name: "Alekhine's defence, chase variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. c4 Nb6 4. c5"
+    },
+    {
+        code: "B02",
+        name: "Alekhine's defence, chase, Mikenas variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. c4 Nb6 4. c5 Nd5 5. Bc4 e6 6. Nc3 d6"
+    },
+    {
+        code: "B03",
+        name: "Alekhine's defence",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4"
+    },
+    {
+        code: "B03",
+        name: "Alekhine's defence, O'Sullivan gambit",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 b5"
+    },
+    {
+        code: "B03",
+        name: "Alekhine's defence",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6"
+    },
+    {
+        code: "B03",
+        name: "Alekhine's defence, Balogh variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. Bc4"
+    },
+    {
+        code: "B03",
+        name: "Alekhine's defence",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. c4"
+    },
+    {
+        code: "B03",
+        name: "Alekhine's defence, exchange variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. c4 Nb6 5. exd6"
+    },
+    {
+        code: "B03",
+        name: "Alekhine's defence, exchange, Karpov variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. c4 Nb6 5. exd6 cxd6 6. Nf3 g6 7. Be2 Bg7 8. O-O O-O 9. h3 Nc6 10. Nc3 Bf5 11. Bf4"
+    },
+    {
+        code: "B03",
+        name: "Alekhine's defence, four pawns attack",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. c4 Nb6 5. f4"
+    },
+    {
+        code: "B03",
+        name: "Alekhine's defence, four pawns attack, Korchnoi variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. c4 Nb6 5. f4 dxe5 6. fxe5 Bf5 7. Nc3 e6 8. Nf3 Be7 9. Be2 O-O 10. O-O f6"
+    },
+    {
+        code: "B03",
+        name: "Alekhine's defence, four pawns attack, 6...Nc6",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. c4 Nb6 5. f4 dxe5 6. fxe5 Nc6"
+    },
+    {
+        code: "B03",
+        name: "Alekhine's defence, four pawns attack, Ilyin-Genevsky var.",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. c4 Nb6 5. f4 dxe5 6. fxe5 Nc6 7. Nf3 Bg4 8. e6 fxe6 9. c5"
+    },
+    {
+        code: "B03",
+        name: "Alekhine's defence, four pawns attack, 7.Be3",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. c4 Nb6 5. f4 dxe5 6. fxe5 Nc6 7. Be3"
+    },
+    {
+        code: "B03",
+        name: "Alekhine's defence, four pawns attack, Tartakower variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. c4 Nb6 5. f4 dxe5 6. fxe5 Nc6 7. Be3 Bf5 8. Nc3 e6 9. Nf3 Qd7 10. Be2 O-O-O 11. O-O Be7"
+    },
+    {
+        code: "B03",
+        name: "Alekhine's defence, four pawns attack, Planinc variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. c4 Nb6 5. f4 g5"
+    },
+    {
+        code: "B03",
+        name: "Alekhine's defence, four pawns attack, fianchetto variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. c4 Nb6 5. f4 g6"
+    },
+    {
+        code: "B03",
+        name: "Alekhine's defence, four pawns attack, Trifunovic variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. c4 Nb6 5. f4 Bf5"
+    },
+    {
+        code: "B04",
+        name: "Alekhine's defence, modern variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. Nf3"
+    },
+    {
+        code: "B04",
+        name: "Alekhine's defence, modern, Larsen variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. Nf3 dxe5"
+    },
+    {
+        code: "B04",
+        name: "Alekhine's defence, modern, Schmid variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. Nf3 Nb6"
+    },
+    {
+        code: "B04",
+        name: "Alekhine's defence, modern, fianchetto variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. Nf3 g6"
+    },
+    {
+        code: "B04",
+        name: "Alekhine's defence, modern, Keres variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. Nf3 g6 5. Bc4 Nb6 6. Bb3 Bg7 7. a4"
+    },
+    {
+        code: "B05",
+        name: "Alekhine's defence, modern variation, 4...Bg4",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. Nf3 Bg4"
+    },
+    {
+        code: "B05",
+        name: "Alekhine's defence, modern, Flohr variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. Nf3 Bg4 5. Be2 c6"
+    },
+    {
+        code: "B05",
+        name: "Alekhine's defence, modern, Panov variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. Nf3 Bg4 5. h3"
+    },
+    {
+        code: "B05",
+        name: "Alekhine's defence, modern, Alekhine variation",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. Nf3 Bg4 5. c4"
+    },
+    {
+        code: "B05",
+        name: "Alekhine's defence, modern, Vitolins attack",
+        moves: "1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. Nf3 Bg4 5. c4 Nb6 6. d5"
+    },
+    {
         code: "B20",
         name: "Sicilian defence",
         moves: "1. e4 c5"
