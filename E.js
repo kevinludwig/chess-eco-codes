014b64272273dacfa817e83b3dff99e014ebcdf4
Kevin Ludwig <kevin.ludwig@gmail.com>
2017-07-06 06:57:11 -0700

    E10-E19

--- E.js
+++ E.js
@@ -90,9 +90,174 @@ module.exports = [
         moves: "1. d4 Nf6 2. c4 e6 3. g3 d5 4. Bg2 Be7 5. Nf3 O-O 6. O-O Nbd7 7. Qc2 c6 8. Nbd2 b6 9. b3 a5 10. Bb2 Ba6"
     },
     {
+        code: "E10",
+        name: "Queen's pawn game",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3"
+    },
+    {
+        code: "E10",
+        name: "Blumenfeld counter-gambit",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 c5 4. d5 b5"
+    },
+    {
+        code: "E10",
+        name: "Blumenfeld counter-gambit accepted",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 c5 4. d5 b5 5. dxe6 fxe6 6. cxb5 d5"
+    },
+    {
+        code: "E10",
+        name: "Blumenfeld counter-gambit, Dus-Chotimursky variation",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 c5 4. d5 b5 5. Bg5"
+    },
+    {
+        code: "E10",
+        name: "Blumenfeld counter-gambit, Spielmann variation",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 c5 4. d5 b5 5. Bg5 exd5 6. cxd5 h6"
+    },
+    {
+        code: "E10",
+        name: "Dzindzikhashvili defence",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 a6"
+    },
+    {
+        code: "E10",
+        name: "Doery defence",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 Ne4"
+    },
+    {
+        code: "E11",
+        name: "Bogo-Indian defence",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 Bb4+"
+    },
+    {
+        code: "E11",
+        name: "Bogo-Indian defence, Gruenfeld variation",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 Bb4+ 4. Nbd2"
+    },
+    {
+        code: "E11",
+        name: "Bogo-Indian defence, Nimzovich variation",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 Bb4+ 4. Bd2 Qe7"
+    },
+    {
+        code: "E11",
+        name: "Bogo-Indian defence, Monticelli trap",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 Bb4+ 4. Bd2 Bxd2+ 5. Qxd2 b6 6. g3 Bb7 7. Bg2 O-O 8. Nc3 Ne4 9. Qc2 Nxc3 10. Ng5"
+    },
+    {
         code: "E12",
         name: "Queen's Indian defence",
         moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6"
+    },
+    {
+        code: "E12",
+        name: "Queen's Indian, Miles variation",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. Bf4"
+    },
+    {
+        code: "E12",
+        name: "Queen's Indian, Petrosian system",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. a3"
+    },
+    {
+        code: "E12",
+        name: "Queen's Indian, 4.Nc3",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. Nc3"
+    },
+    {
+        code: "E12",
+        name: "Queen's Indian, 4.Nc3, Botvinnik variation",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. Nc3 Bb7 5. Bg5 h6 6. Bh4 g5 7. Bg3 Nh5"
+    },
+    {
+        code: "E13",
+        name: "Queen's Indian, 4.Nc3, main line",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. Nc3 Bb7 5. Bg5 h6 6. Bh4 Bb4"
+    },
+    {
+        code: "E14",
+        name: "Queen's Indian, 4.e3",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. e3"
+    },
+    {
+        code: "E14",
+        name: "Queen's Indian, Averbakh variation",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. e3 Bb7 5. Bd3 c5 6. O-O Be7 7. b3 O-O 8. Bb2 cxd4 9. Nxd4"
+    },
+    {
+        code: "E15",
+        name: "Queen's Indian, 4.g3",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. g3"
+    },
+    {
+        code: "E15",
+        name: "Queen's Indian, Nimzovich variation 4. g3 Ba6",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. g3 Ba6"
+    },
+    {
+        code: "E15",
+        name: "Queen's Indian, 4.g3 Bb7",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. g3 Bb7"
+    },
+    {
+        code: "E15",
+        name: "Queen's Indian, Rubinstein variation",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. g3 Bb7 5. Bg2 c5 6. d5 exd5 7. Nh4"
+    },
+    {
+        code: "E15",
+        name: "Queen's Indian, Buerger variation",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. g3 Bb7 5. Bg2 c5 6. d5 exd5 7. Ng5"
+    },
+    {
+        code: "E16",
+        name: "Queen's Indian, Capablanca variation",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. g3 Bb7 5. Bg2 Bb4+"
+    },
+    {
+        code: "E16",
+        name: "Queen's Indian, Yates variation",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. g3 Bb7 5. Bg2 Bb4+ 6. Bd2 a5"
+    },
+    {
+        code: "E16",
+        name: "Queen's Indian, Riumin variation",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. g3 Bb7 5. Bg2 Bb4+ 6. Bd2 Be7"
+    },
+    {
+        code: "E17",
+        name: "Queen's Indian, 5.Bg2 Be7",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. g3 Bb7 5. Bg2 Be7"
+    },
+    {
+        code: "E17",
+        name: "Queen's Indian, anti-Queen's Indian system",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. g3 Bb7 5. Bg2 Be7 6. Nc3"
+    },
+    {
+        code: "E17",
+        name: "Queen's Indian, Opovcensky variation",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. g3 Bb7 5. Bg2 Be7 6. Nc3 Ne4 7. Bd2"
+    },
+    {
+        code: "E17",
+        name: "Queen's Indian, old main line, 6.O-O",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. g3 Bb7 5. Bg2 Be7 6. O-O"
+    },
+    {
+        code: "E17",
+        name: "Queen's Indian, Euwe variation",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. g3 Bb7 5. Bg2 Be7 6. O-O O-O 7. b3"
+    },
+    {
+        code: "E18",
+        name: "Queen's Indian, old main line, 7.Nc3",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. g3 Bb7 5. Bg2 Be7 6. O-O O-O 7. Nc3"
+    },
+    {
+        code: "E19",
+        name: "Queen's Indian, old main line, 9.Qxc3",
+        moves: "1. d4 Nf6 2. c4 e6 3. Nf3 b6 4. g3 Bb7 5. Bg2 Be7 6. O-O O-O 7. Nc3 Ne4 8. Qc2 Nxc3 9. Qxc3"
     },
     {
         code: "E20",
