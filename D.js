e2ae3a22f6b7ed5a32dd615961a0e779383d2518
Kevin Ludwig <kevin.ludwig@gmail.com>
2017-07-06 22:34:03 -0700

    D00-D14

--- D.js
+++ D.js
@@ -1,8 +1,273 @@
 module.exports = [
+    {
+        code: "D00",
+        name: "Queen's pawn game",
+        moves: "1. d4 d5"
+    },
+    {
+        code: "D00",
+        name: "Queen's pawn, Mason variation",
+        moves: "1. d4 d5 2. Bf4"
+    },
+    {
+        code: "D00",
+        name: "Queen's pawn, Mason variation, Steinitz counter-gambit",
+        moves: "1. d4 d5 2. Bf4 c5"
+    },
+    {
+        code: "D00",
+        name: "Levitsky attack (Queen's bishop attack)",
+        moves: "1. d4 d5 2. Bg5"
+    },
+    {
+        code: "D00",
+        name: "Blackmar gambit",
+        moves: "1. d4 d5 2. e4"
+    },
+    {
+        code: "D00",
+        name: "Queen's pawn, stonewall attack",
+        moves: "1. d4 d5 2. e3 Nf6 3. Bd3"
+    },
+    {
+        code: "D00",
+        name: "Queen's pawn, Chigorin variation",
+        moves: "1. d4 d5 2. Nc3"
+    },
+    {
+        code: "D00",
+        name: "Queen's pawn, Anti-Veresov",
+        moves: "1. d4 d5 2. Nc3 Bg4"
+    },
+    {
+        code: "D00",
+        name: "Blackmar-Diemer gambit",
+        moves: "1. d4 d5 2. Nc3 Nf6 3. e4"
+    },
+    {
+        code: "D00",
+        name: "Blackmar-Diemer, Euwe defence",
+        moves: "1. d4 d5 2. Nc3 Nf6 3. e4 dxe4 4. f3 exf3 5. Nxf3 e6"
+    },
+    {
+        code: "D00",
+        name: "Blackmar-Diemer, Lemberg counter-gambit",
+        moves: "1. d4 d5 2. Nc3 Nf6 3. e4 e5"
+    },
+    {
+        code: "D01",
+        name: "Richter-Veresov attack",
+        moves: "1. d4 d5 2. Nc3 Nf6 3. Bg5"
+    },
+    {
+        code: "D01",
+        name: "Richter-Veresov attack, Veresov variation",
+        moves: "1. d4 d5 2. Nc3 Nf6 3. Bg5 Bf5 4. Bxf6"
+    },
+    {
+        code: "D01",
+        name: "Richter-Veresov attack, Richter variation",
+        moves: "1. d4 d5 2. Nc3 Nf6 3. Bg5 Bf5 4. f3"
+    },
+    {
+        code: "D02",
+        name: "Queen's pawn game",
+        moves: "1. d4 d5 2. Nf3"
+    },
+    {
+        code: "D02",
+        name: "Queen's pawn game, Chigorin variation",
+        moves: "1. d4 d5 2. Nf3 Nc6"
+    },
+    {
+        code: "D02",
+        name: "Queen's pawn game, Krause variation",
+        moves: "1. d4 d5 2. Nf3 c5"
+    },
+    {
+        code: "D02",
+        name: "Queen's pawn game",
+        moves: "1. d4 d5 2. Nf3 Nf6"
+    },
+    {
+        code: "D02",
+        name: "Queen's bishop game",
+        moves: "1. d4 d5 2. Nf3 Nf6 3. Bf4"
+    },
+    {
+        code: "D03",
+        name: "Torre attack (Tartakower variation)",
+        moves: "1. d4 d5 2. Nf3 Nf6 3. Bg5"
+    },
+    {
+        code: "D04",
+        name: "Queen's pawn game",
+        moves: "1. d4 d5 2. Nf3 Nf6 3. e3"
+    },
+    {
+        code: "D05",
+        name: "Queen's pawn game",
+        moves: "1. d4 d5 2. Nf3 Nf6 3. e3 e6"
+    },
+    {
+        code: "D05",
+        name: "Queen's pawn game, Zukertort variation",
+        moves: "1. d4 d5 2. Nf3 Nf6 3. e3 e6 4. Nbd2 c5 5. b3"
+    },
+    {
+        code: "D05",
+        name: "Queen's pawn game",
+        moves: "1. d4 d5 2. Nf3 Nf6 3. e3 e6 4. Bd3"
+    },
+    {
+        code: "D05",
+        name: "Queen's pawn game, Rubinstein (Colle-Zukertort) variation",
+        moves: "1. d4 d5 2. Nf3 Nf6 3. e3 e6 4. Bd3 c5 5. b3"
+    },
+    {
+        code: "D05",
+        name: "Colle system",
+        moves: "1. d4 d5 2. Nf3 Nf6 3. e3 e6 4. Bd3 c5 5. c3"
+    },
+    {
+        code: "D06",
+        name: "Queen's Gambit",
+        moves: "1. d4 d5 2. c4"
+    },
+    {
+        code: "D06",
+        name: "Queen's Gambit Declined, Grau (Sahovic) defence",
+        moves: "1. d4 d5 2. c4 Bf5"
+    },
+    {
+        code: "D06",
+        name: "Queen's Gambit Declined, Marshall defence",
+        moves: "1. d4 d5 2. c4 Nf6"
+    },
+    {
+        code: "D06",
+        name: "Queen's Gambit Declined, symmetrical (Austrian) defence",
+        moves: "1. d4 d5 2. c4 c5"
+    },
     {   
         code: "D07",
         name: "Queen's Gambit Declined, Chigorin defence",
         moves: "1. d4 d5 2. c4 Nc6"
+    },
+    {
+        code: "D07",
+        name: "Queen's Gambit Declined, Chigorin defence, Janowski variation",
+        moves: "1. d4 d5 2. c4 Nc6 3. Nc3 dxc4 4. Nf3"
+    },
+    {
+        code: "D08",
+        name: "Queen's Gambit Declined, Albin counter-gambit",
+        moves: "1. d4 d5 2. c4 e5"
+    },
+    {
+        code: "D08",
+        name: "Queen's Gambit Declined, Albin counter-gambit, Lasker trap",
+        moves: "1. d4 d5 2. c4 e5 3. dxe5 d4 4. e3 Bb4+ 5. Bd2 dxe3"
+    },
+    {
+        code: "D08",
+        name: "Queen's Gambit Declined, Albin counter-gambit",
+        moves: "1. d4 d5 2. c4 e5 3. dxe5 d4 4. Nf3"
+    },
+    {
+        code: "D08",
+        name: "Queen's Gambit Declined, Albin counter-gambit, Alapin variation",
+        moves: "1. d4 d5 2. c4 e5 3. dxe5 d4 4. Nf3 Nc6 5. Nbd2"
+    },
+    {
+        code: "D08",
+        name: "Queen's Gambit Declined, Albin counter-gambit, Krenosz variation",
+        moves: "1. d4 d5 2. c4 e5 3. dxe5 d4 4. Nf3 Nc6 5. Nbd2 Bg4 6. h3 Bxf3 7. Nxf3 Bb4+ 8. Bd2 Qe7"
+    },
+    {
+        code: "D08",
+        name: "Queen's Gambit Declined, Albin counter-gambit, Janowski variation",
+        moves: "1. d4 d5 2. c4 e5 3. dxe5 d4 4. Nf3 Nc6 5. Nbd2 f6"
+    },
+    {
+        code: "D08",
+        name: "Queen's Gambit Declined, Albin counter-gambit, Balogh variation",
+        moves: "1. d4 d5 2. c4 e5 3. dxe5 d4 4. Nf3 Nc6 5. Nbd2 Qe7"
+    },
+    {
+        code: "D09",
+        name: "Queen's Gambit Declined, Albin counter-gambit, 5.g3",
+        moves: "1. d4 d5 2. c4 e5 3. dxe5 d4 4. Nf3 Nc6 5. g3"
+    },
+    {
+        code: "D10",
+        name: "Queen's Gambit Declined Slav defence",
+        moves: "1. d4 d5 2. c4 c6"
+    },
+    {
+        code: "D10",
+        name: "Queen's Gambit Declined Slav defence, Alekhine variation",
+        moves: "1. d4 d5 2. c4 c6 3. Nc3 dxc4 4. e4"
+    },
+    {
+        code: "D10",
+        name: "Queen's Gambit Declined Slav, Winawer counter-gambit",
+        moves: "1. d4 d5 2. c4 c6 3. Nc3 e5"
+    },
+    {
+        code: "D10",
+        name: "Queen's Gambit Declined Slav defence, exchange variation",
+        moves: "1. d4 d5 2. c4 c6 3. cxd5"
+    },
+    {
+        code: "D11",
+        name: "Queen's Gambit Declined Slav, 3.Nf3",
+        moves: "1. d4 d5 2. c4 c6 3. Nf3"
+    },
+    {
+        code: "D11",
+        name: "Queen's Gambit Declined Slav, Breyer variation",
+        moves: "1. d4 d5 2. c4 c6 3. Nf3 Nf6 4. Nbd2"
+    },
+    {
+        code: "D11",
+        name: "Queen's Gambit Declined Slav, 4.e3",
+        moves: "1. d4 d5 2. c4 c6 3. Nf3 Nf6 4. e3"
+    },
+    {
+        code: "D12",
+        name: "Queen's Gambit Declined Slav, 4.e3 Bf5",
+        moves: "1. d4 d5 2. c4 c6 3. Nf3 Nf6 4. e3 Bf5"
+    },
+    {
+        code: "D12",
+        name: "Queen's Gambit Declined Slav, Landau variation",
+        moves: "1. d4 d5 2. c4 c6 3. Nf3 Nf6 4. e3 Bf5 5. cxd5 cxd5 6. Qb3 Qc8 7. Bd2 e6 8. Na3"
+    },
+    {
+        code: "D12",
+        name: "Queen's Gambit Declined Slav, exchange variation",
+        moves: "1. d4 d5 2. c4 c6 3. Nf3 Nf6 4. e3 Bf5 5. cxd5 cxd5 6. Nc3"
+    },
+    {
+        code: "D12",
+        name: "Queen's Gambit Declined Slav, Amsterdam variation",
+        moves: "1. d4 d5 2. c4 c6 3. Nf3 Nf6 4. e3 Bf5 5. cxd5 cxd5 6. Nc3 e6 7. Ne5 Nfd7"
+    },
+    {
+        code: "D13",
+        name: "Queen's Gambit Declined Slav, exchange variation",
+        moves: "1. d4 d5 2. c4 c6 3. Nf3 Nf6 4. cxd5 cxd5"
+    },
+    {
+        code: "D14",
+        name: "Queen's Gambit Declined Slav, exchange variation, 6.Bf4 Bf5",
+        moves: "1. d4 d5 2. c4 c6 3. Nf3 Nf6 4. cxd5 cxd5 5. Nc3 Nc6 6. Bf4 Bf5"
+    },
+    {
+        code: "D14",
+        name: "Queen's Gambit Declined Slav, exchange, Trifunovic variation",
+        moves: "1. d4 d5 2. c4 c6 3. Nf3 Nf6 4. cxd5 cxd5 5. Nc3 Nc6 6. Bf4 Bf5 7. e3 e6 8. Qb3 Bb4"
     },
     {
         code: "D20",
