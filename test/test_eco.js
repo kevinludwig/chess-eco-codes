0e8242f8406ef713fb4178c8a1c48070ae588431
vjuneja <vjuneja@ebay.com>
2018-10-02 23:54:28 -0700

    codes and test

--- test/test_eco.js
+++ test/test_eco.js
@@ -6,4 +6,7 @@ describe('eco codes', () => {
     it('should lookup by eco code', () => {
         eco('rnbqkbnr/pppppppp/8/8/6P1/8/PPPPPP1P/RNBQKBNR b KQkq g3 0 1').name.should.be.eql("Grob's Attack");
     });
+    it('should return null in opening state', () => {
+        should.not.exist(eco('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'));
+    });
 });
