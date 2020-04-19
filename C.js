d92e724b4031fdc109d66d477af28b5ebdf3b082
Kevin Ludwig <kevin.ludwig@gmail.com>
2020-04-18 20:04:13 -0700

    add c00

--- C.js
+++ C.js
@@ -1,1 +1,97 @@
-module.exports = [];
+module.exports = [
+    {
+        code: "C00",
+        name: "French defense",
+        moves: "1. e4 e6"
+    },
+    {
+        code: "C00",
+        name: "French defense, Steiner variation",
+        moves: "1. e4 e6 2. c4"
+    },
+    {
+        code: "C00",
+        name: "French defense, Reti (Spielmann) variation",
+        moves: "1. e4 e6 2. b3"
+    },
+    {
+        code: "C00",
+        name: "French defense, Steinitz attack",
+        moves: "1. e4 e6 2. e5"
+    },
+    {
+        code: "C00",
+        name: "French defense, Labourdonnais variation",
+        moves: "1. e4 e6 2. f4"
+    },
+    {
+        code: "C00",
+        name: "French defense",
+        moves: "1. e4 e6 2. Nf3"
+    },
+    {
+        code: "C00",
+        name: "French defense, Wing gambit",
+        moves: "1. e4 e6 2. Nf3 d5 3. e5 c5 4. b4"
+    },
+    {
+        code: "C00",
+        name: "French defense",
+        moves: "1. e4 e6 2. Nc3"
+    },
+    {
+        code: "C00",
+        name: "French, Pelican variation",
+        moves: "1. e4 e6 2. Nc3 d5 3. f4"
+    },
+    {
+        code: "C00",
+        name: "French, Two knights variation",
+        moves: "1. e4 e6 2. Nc3 d5 3. Nf3"
+    },
+    {
+        code: "C00",
+        name: "French, Chigorin variation",
+        moves: "1. e4 e6 2. Qe2"
+    },
+    {
+        code: "C00",
+        name: "French, King's Indian attack",
+        moves: "1. e4 e6 2. d3"
+    },
+    {
+         code: "C00",
+         name: "French, Reversed Philidor formation",
+         moves: "1. e4 e6 2. d3 d5 3. Nd2 Nf6 4. Ngf3 Nc6 5. Be2"
+    },
+    {
+        code: "C00",
+        name: "C00 French defence",
+        moves: "1. e4 e6 2. d4"
+    },
+    {
+        code: "C00",
+        name: "Lengfellner system",
+        moves: "1. e4 e6 2. d4 d6"
+    },
+    {
+        code: "C00",
+        name: "St. George defense",
+        moves: "1. e4 e6 2. d4 a6"
+    },
+    {
+        code: "C00",
+        name: "French defence",
+        moves: "1. e4 e6 2. d4 d5"
+    },
+    {
+        code: "C00",
+        name: "French, Schlechter variation",
+        moves: "1. e4 e6 2. d4 d5 3. Bd3"
+    },
+    {
+        code: "C00",
+        name: "French, Alapin variation",
+        moves: "1. e4 e6 2. d4 d5 3. Be3"
+    }
+];
