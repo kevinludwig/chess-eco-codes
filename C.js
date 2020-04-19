60133dffe2b54172c9e54e2b1cf75fd765253dc7
Kevin Ludwig <kevin.ludwig@gmail.com>
2020-04-19 09:11:55 -0700

    add c01-c11

--- C.js
+++ C.js
@@ -93,5 +93,225 @@ module.exports = [
         code: "C00",
         name: "French, Alapin variation",
         moves: "1. e4 e6 2. d4 d5 3. Be3"
+    },
+    {
+        code: "C01",
+        name: "French, exchange variation",
+        moves: "1. e4 e6 2. d4 d5 3. exd5"
+    },
+    {
+        code: "C01",
+        name: "French exchange, Svenonius variation",
+        moves: "1. e4 e6 2. d4 d5 3. exd5 exd5 4. Nc3 Nf6 5. Bg5"
+    },
+    {
+        code: "C01",
+        name: "French, exchange, Bogolyubov variation",
+        moves: "1. e4 e6 2. d4 d5 3. exd5 exd5 4. Nc3 Nf6 5. Bg5 Nc6"
+    },
+    {
+        code: "C02",
+        name: "French, advance variation",
+        moves: "1. e4 e6 2. d4 d5 3. e5"
+    },
+    {
+        code: "C02",
+        name: "French, advance, Steinitz variation",
+        moves: "1. e4 e6 2. d4 d5 3. e5 c5 4. dxc5"
+    },
+    {
+        code: "C02",
+        name: "French, advance, Nimzovich variation",
+        moves: "1. e4 e6 2. d4 d5 3. e5 c5 4. Qg4"
+    },
+    {
+        code: "C02",
+        name: "French, advance, Nimzovich system",
+        moves: "1. e4 e6 2. d4 d5 3. e5 c5 4. Nf3"
+    },
+    {
+        code: "C02",
+        name: "French, advance variation",
+        moves: "1. e4 e6 2. d4 d5 3. e5 c5 4. c3"
+    },
+    {
+        code: "C02",
+        name: "French, advance, Wade variation",
+        moves: "1. e4 e6 2. d4 d5 3. e5 c5 4. c3 Qb6 5. Nf3 Bd7"
+    },
+    {
+        code: "C02",
+        name: "C02 French, advance variation",
+        moves: "1. e4 e6 2. d4 d5 3. e5 c5 4. c3 Nc6"
+    },
+    {
+        code: "C02",
+        name: "French, advance, Paulsen attack",
+        moves: "1. e4 e6 2. d4 d5 3. e5 c5 4. c3 Nc6 5. Nf3"
+    },
+    {
+        code: "C02",
+        name: "French, advance, Milner-Barry gambit",
+        moves: "1. e4 e6 2. d4 d5 3. e5 c5 4. c3 Nc6 5. Nf3 Qb6 6. Bd3"
+    },
+    {
+        code: "C02",
+        name: "French, advance, Euwe variation",
+        moves: "1. e4 e6 2. d4 d5 3. e5 c5 4. c3 Nc6 5. Nf3 Bd7"
+    },
+    {
+        code: "C03",
+        name: "French, Tarrasch",
+        moves: "1. e4 e6 2. d4 d5 3. Nd2"
+    },
+    {
+        code: "C03",
+        name: "French, Tarrasch, Haberditz variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nd2 f5"
+    },
+    {
+        code: "C03",
+        name: "French, Tarrasch, Guimard variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nd2 Nc6"
+    },
+    {
+        code: "C04",
+        name: "French, Tarrasch, Guimard main line",
+        moves: "1. e4 e6 2. d4 d5 3. Nd2 Nc6 4. Ngf3 Nf6"
+    },
+    {
+        code: "C05",
+        name: "French, Tarrasch, closed variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nd2 Nf6"
+    },
+    {
+        code: "C05",
+        name: "French, Tarrasch, Botvinnik variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nd2 Nf6 4. e5 Nfd7 5. Bd3 c5 6. c3 b6"
+    },
+    {
+        code: "C05",
+        name: "French, Tarrasch, closed variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nd2 Nf6 4. e5 Nfd7 5. Bd3 c5 6. c3 Nc6"
+    },
+    {
+        code: "C06",
+        name: "French, Tarrasch, closed variation, main line",
+        moves: "1. e4 e6 2. d4 d5 3. Nd2 Nf6 4. e5 Nfd7 5. Bd3 c5 6. c3 Nc6 7. Ne2 cxd4 8. cxd4"
+    },
+    {
+        code: "C06",
+        name: "French, Tarrasch, Leningrad variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nd2 Nf6 4. e5 Nfd7 5. Bd3 c5 6. c3 Nc6 7. Ne2 cxd4 8. cxd4 Nb6"
+    },
+    {
+        code: "C07",
+        name: "French, Tarrasch, open variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nd2 c5"
+    },
+    {
+        code: "C07",
+        name: "French, Tarrasch, Eliskases variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nd2 c5 4. exd5 Qxd5 5. Ngf3 cxd4 6. Bc4 Qd8"
+    },
+    {
+        code: "C08",
+        name: "French, Tarrasch, open, 4.ed ed",
+        moves: "1. e4 e6 2. d4 d5 3. Nd2 c5 4. exd5 exd5"
+    },
+    {
+        code: "C09",
+        name: "French, Tarrasch, open variation, main line",
+        moves: "1. e4 e6 2. d4 d5 3. Nd2 c5 4. exd5 exd5 5. Ngf3 Nc6"
+    },
+    {
+         code: "C10",
+         name: "French, Paulsen variation",
+         moves: "1. e4 e6 2. d4 d5 3. Nc3"
+    },
+    {
+        code: "C10",
+        name: "French, Marshall variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 c5"
+    },
+    {
+        code: "C10",
+        name: "French, Rubinstein variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 dxe4"
+    },
+    {
+        code: "C10",
+        name: "French, Fort Knox variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 dxe4 4. Nxe4 Bd7 5. Nf3 Bc6"
+    },
+    {
+        code: "C10",
+        name: "French, Rubinstein variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 dxe4 4. Nxe4 Nd7"
+    },
+    {
+        code: "C10",
+        name: "French, Rubinstein, Capablanca line",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 dxe4 4. Nxe4 Nd7 5. Nf3 Ngf6 6. Nxf6+ Nxf6 7. Ne5"
+    },
+    {
+        code: "C10",
+        name: "French, Frere (Becker) variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 dxe4 4. Nxe4 Qd5"
+    },
+    {
+        code: "C11",
+        name: "French defence",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 Nf6"
+    },
+    {
+        code: "C11",
+        name: "French, Swiss variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 Nf6 4. Bd3"
+    },
+    {
+        code: "C11",
+        name: "French, Henneberger variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 Nf6 4. Be3"
+    },
+    {
+        code: "C11",
+        name: "French, Steinitz variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 Nf6 4. e5"
+    },
+    {
+        code: "C11",
+        name: "French, Steinitz, Bradford attack",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 Nf6 4. e5 Nfd7 5. f4 c5 6. dxc5 Bxc5 7. Qg4"
+    },
+    {
+        code: "C11",
+        name: "French, Steinitz variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 Nf6 4. e5 Nfd7 5. f4 c5 6. dxc5 Nc6"
+    },
+    {
+        code: "C11",
+        name: "French, Steinitz, Brodsky-Jones variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 Nf6 4. e5 Nfd7 5. f4 c5 6. dxc5 Nc6 7. a3 Bxc5 8. Qg4 O-O 9. Nf3 f6"
+    },
+    {
+        code: "C11",
+        name: "French, Steinitz variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 Nf6 4. e5 Nfd7 5. f4 c5 6. Nf3"
+    },
+    {
+        code: "C11",
+        name: "French, Steinitz, Boleslavsky variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 Nf6 4. e5 Nfd7 5. f4 c5 6. Nf3 Nc6 7. Be3"
+    },
+    {
+        code: "C11",
+        name: "French, Steinitz, Gledhill attack",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 Nf6 4. e5 Nfd7 5. Qg4"
+    },
+    {
+        code: "C11",
+        name: "French, Burn variation",
+        moves: "1. e4 e6 2. d4 d5 3. Nc3 Nf6 4. Bg5 dxe4"
     }
 ];
